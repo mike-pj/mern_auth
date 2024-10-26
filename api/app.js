c52b6c0a5a283e14591e1c232ade0f3438681473
Michael Uzoigwe <michaeluzoigwe78@gmail.com>
2024-10-26 17:05:06 +0100

    add a middleware and a function to handle errors

--- api/app.js
+++ api/app.js
@@ -26,3 +26,13 @@ app.listen(port, () => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal message Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
--- api/contollers/authController.js
+++ api/contollers/authController.js
@@ -2,7 +2,7 @@ import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
 import { signupValidation } from "../middlewares/validationForms.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   try {
     const { error } = signupValidation(req.body)
     if(error) {
@@ -22,9 +22,8 @@ export const signup = async (req, res) => {
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
 
-
     res.status(200).json({ message: "User created successfully" });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
